281a762ee98c491a70a3f34e7d0a55d74579eddc
Maryse RAJAONARY <mrajaona@e1r3p14.42.fr>
2018-10-16 10:51:51 +0200

    lis

--- rendu/avaj/elements/weathertower/Tower.java
+++ rendu/avaj/elements/weathertower/Tower.java
@@ -3,10 +3,12 @@ package avaj.elements.weathertower;
 import avaj.elements.flyable.Flyable;
 import avaj.elements.aircraft.Coordinates;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Tower {
 
-    private Flyable observers;
-    // private Flyable* observers;
+    private List <Flyable> observers = new ArrayList <Flyable> ();
 
     public void register(Flyable flyable) {
 
