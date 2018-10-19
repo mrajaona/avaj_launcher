0a89c50544cf8561aeaf56ceb53c0a8828c9211a
Maryse RAJAONARY <mrajaona@e1r3p8.42.fr>
2018-10-19 13:21:46 +0200

    better compiler

--- rendu/compile.sh
+++ rendu/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /usr/bin/find . -name '*.java' > sources.txt \
-&& /usr/bin/javac -verbose -sourcepath @sources.txt
+&& /usr/bin/javac -d . @sources.txt
+# && /usr/bin/javac -verbose -sourcepath @sources.txt
